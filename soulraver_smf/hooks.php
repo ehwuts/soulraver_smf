069ea527d6262157162e7e677d68fbdefed81eaa
ehwuts matsu <tsukan.git@gmail.com>
2019-02-16 08:05:47 -0800

    cache partial functionality restructure

--- soulraver_smf/hooks.php
+++ soulraver_smf/hooks.php
@@ -14,7 +14,6 @@ $hooks = array(
 	'integrate_pre_include' => '$sourcedir/Subs-soulraver_smf.php',
 	'integrate_general_mod_settings' => 'soulraver_smf_configs',
 	'integrate_load_theme' => 'soulraver_smf_load_theme',
-	'integrate_bbc_buttons' => 'soulraver_smf_add_buttons',
 	'integrate_bbc_codes'   => 'soulraver_smf_add_codes'
 );
 
