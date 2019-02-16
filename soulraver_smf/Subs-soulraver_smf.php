442a00b65349cb5acd53bca666186a7ada0dd678
ehwuts matsu <tsukan.git@gmail.com>
2019-02-16 08:23:38 -0800

    rip example button registration code in the hopes it works

--- soulraver_smf/Subs-soulraver_smf.php
+++ soulraver_smf/Subs-soulraver_smf.php
@@ -40,26 +40,64 @@ function soulraver_smf_load_theme() {
 }
 
 /* integrate_bbc_buttons */
+/* aside from the function name and our values for $newbuttons, this function's code is sourced from
+ * https://github.com/SimpleMachines/SMF-Customization-Team-mods/blob/ce8d9b3c4439ec0f50a0731da12313bfa3304f88/BBC_Overline/Subs-Overlinebbc.php#L42
+ * and is @copyright 2011 Simple Machines and licensed under the 3-clause BSD as described at http://www.simplemachines.org/about/smf/license.php
+ */
 function soulraver_smf_add_buttons(&$buttons) {
 	global $txt;
 	
-	/* TODO: Make the buttons actually appear */
-	$buttons[count($buttons) - 1][] = array(
-		'image'       => 'zomg_spoiler',
-		'code'        => 'spoiler',
-		'before'      => '[spoiler]',
-		'after'       => '[/spoiler]',
-		'description' => $txt['spoiler']
+	$row_position = 1; // Which row are we adding the buttons on (starting at 1)
+	$col_position = 4; // which col are we inserting the buttons
+	
+	// Define the new buttons
+	$newbuttons = array(
+		array(
+			'image'       => 'zomg_spoiler',
+			'code'        => 'spoiler',
+			'before'      => '[spoiler]',
+			'after'       => '[/spoiler]',
+			'description' => $txt['spoiler']
+		),
+		/*
+		array(
+			'image'       => 'mythic_die',
+			'code'        => 'roll',
+			'before'      => '[roll]',
+			'after'       => '[/roll]',
+			'description' => $txt['roll']
+		)*/
 	);
 	
-	//$buttons[count($buttons) - 1][] = array(
-	//	'image'       => 'mythic_die',
-	//	'code'        => 'roll',
-	//	'before'      => '[roll]',
-	//	'after'       => '[/roll]',
-	//	'description' => $txt['roll']
-	//);
+	// Move from x,y coordinates to array values
+	$row_position--;
+	$col_position--;
 	
+	// Get the individual button rows
+	foreach ($buttons as $sub_buttons)
+		$button_row[] = $sub_buttons;
+		
+	// set the row bounds, not less than 0, not more than an extra row
+	$total_rows = count($buttons);
+	$row_position = min(max($row_position,0),$total_rows);
+	
+	// If the user specified a new row add one
+	if ($row_position == $total_rows)
+	{
+		$button_row[$row_position] = array();
+		$total_rows++;
+	}
+	// Set the col bounds, not less than 0 not more than the number of columns in this row
+	$total_cols = count($button_row[$row_position]);
+	$col_position = min(max($col_position,0),$total_cols);
+		
+	// Insert the new buttons in to the row and col specified
+	array_splice($button_row[$row_position], $col_position, $total_cols, array_merge($newbuttons, array_slice($button_row[$row_position], $col_position))); 
+	// join the button array back together
+	$buttons = array();
+	for ($i = 0; $i < $total_rows; $i++)
+		$buttons[] = $button_row[$i];
+	return;
 }
 
 /* integrate_bbc_codes */
